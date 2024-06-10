ee8044b1b6b318c025d5cf6b0d60ca264b580da8
tobiaseisinger <tobias.eisinger@htlstp.at>
2024-06-10 13:27:28 +0200

    added texture

--- TestGame/Game1.cs
+++ TestGame/Game1.cs
@@ -6,8 +6,11 @@ namespace TestGame;
 
 public class Game1 : Game
 {
+    Texture2D barrierTexture;
+
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+
 
     public Game1()
     {
@@ -28,6 +31,7 @@ public class Game1 : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
+        barrierTexture = Content.Load<Texture2D>("barrier");
     }
 
     protected override void Update(GameTime gameTime)
@@ -45,6 +49,9 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(barrierTexture, new Vector2(0,0), Color.White);
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
